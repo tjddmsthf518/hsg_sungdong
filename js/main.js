90e5469598ae94a7e2743a895216309c89345958
tjddmsthf518 <tjddmsthf518@gmail.com>
2025-11-07 12:50:13 +0900

    sungdong_25_11_07

--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
 $(document).ready(function () {
+
+  // $(".top_btn").click(function(){
+  //   $('body, html').scrollTop({
+  //     top: 0
+  //   });
+  // });
+
+  $(".top_btn").click(function(){
+    $("body, html").stop().animate({
+      scrollTop: 0
+    });
+  });
+
+  $(".menu_bar").click(function(){
+    $(".sub_menu_wrap").stop().animate({
+      right: 0
+    });
+
+    $("body").css({
+      overflowY: "hidden"
+    });
+  });
+
+  $(".close_btn").click(function(){
+    $(".sub_menu_wrap").stop().animate({
+      right: "-100%"
+    });
+
+    $("body").css({
+      overflowY: "unset"
+    });
+  });
+  
+  // $(window).scroll(function(){
+  //   var pageY = $(window).scrollTop();
+  // });
+
   var swiper = new Swiper(".con_wrap_4", {
     scrollbar: {
       el: ".swiper-scrollbar",
